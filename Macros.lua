3fc839283c4e5bf35b63d30b83c7a13d3f1ae570
Joshua Harms <joshua@nozzlegear.com>
2022-03-05 09:02:03 -0600

    Handle cases where the Covenant ID is nil
    This can happen when the game hasn't registered the player's covenant before the addon loads

--- Macros.lua
+++ Macros.lua
@@ -1,5 +1,21 @@
 local ADDON_NAME, Mod = ...
 
+local PHIAL_ITEM_ID = 177278
+
+-- Macro event listeners
+local macroFrame = CreateFrame("Frame")
+macroFrame:RegisterEvent("ADDON_LOADED")
+macroFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+macroFrame:RegisterEvent("COVENANT_CHOSEN")
+macroFrame:RegisterEvent("SPELLS_CHANGED")
+macroFrame:RegisterEvent("BAG_UPDATE_DELAYED")
+macroFrame:RegisterEvent("UPDATE_MACROS")
+macroFrame:SetScript("OnEvent", function (self, event, ...)
+    local handler = self[event]
+
+    Mod.UpdateGeneralCovenantMacroIcon(self, event, ...)
+end)
+
 -- Gets the Cov_Gen_DJUI macro index. Returns nil if it can't be found.
 local function GetGeneralCovenantMacroIndex()
     for i = 1, GetNumMacros() do
@@ -11,8 +27,8 @@ local function GetGeneralCovenantMacroIndex()
     return nil
 end
 
-local macroIndex
-local covId
+local GEN_MACRO_INDEX
+local PLAYER_COVENANT_ID
 
 function Mod.UpdateGeneralCovenantMacroIcon(self, event, ...)
     if event == "ADDON_LOADED" then
@@ -23,18 +39,27 @@ function Mod.UpdateGeneralCovenantMacroIcon(self, event, ...)
         end
 
         -- Get the index of the General Covenant macro once and only update it when macros change
-        macroIndex = GetGeneralCovenantMacroIndex()
+        GEN_MACRO_INDEX = GetGeneralCovenantMacroIndex()
         -- We get the covenant id here as well because it can be 0 (none) when the module loads
-        covId = C_Covenants.GetActiveCovenantID()
+        PLAYER_COVENANT_ID = C_Covenants.GetActiveCovenantID()
 
         -- Unregister the ADDON_LOADED event
         self:UnregisterEvent("ADDON_LOADED")
+    elseif event == "PLAYER_ENTERING_WORLD" then
+        -- Sometimes the covenant id still isn't set after ADDON_LOADED. Ensure it is set here.
+        if PLAYER_COVENANT_ID == 0 or PLAYER_COVENANT_ID == nil then
+            print("Attempting to get player covenant id for a second time")
+            PLAYER_COVENANT_ID = C_Covenants.GetActiveCovenantID()
+        end
+
+        -- Unregister this event, we don't need to check this each time the player enters a loading screen
+        self:UnregisterEvent("PLAYER_ENTERING_WORLD")
     elseif event == "UPDATE_MACROS" then
         -- The player's macros have changed, update the macro index
-        macroIndex = GetGeneralCovenantMacroIndex()
+        GEN_MACRO_INDEX = GetGeneralCovenantMacroIndex()
     elseif event == "COVENANT_CHOSEN" then
         -- The player has changed covenants, update the covenant id
-        covId = ...
+        PLAYER_COVENANT_ID = ...
         -- This event fires before the player's spellbook has been updated with the new covenant spells, so we can't update the macro yet
         return
     elseif event == "SPELLS_CHANGED" then
@@ -42,34 +67,26 @@ function Mod.UpdateGeneralCovenantMacroIcon(self, event, ...)
         if IsInInstance() then
             return
         end
-    elseif event == "BAG_UPDATE_DELAYED" and covId ~= 1 then
+    elseif event == "BAG_UPDATE_DELAYED" and PLAYER_COVENANT_ID ~= 1 then
         -- Bag was updated but the player is not Kyrian so it's not relevant
         return
     end
 
-    if macroIndex then
-        if covId == 1 then
+    if GEN_MACRO_INDEX then
+        if PLAYER_COVENANT_ID == 1 then
             -- Kyrian, set the macro icon to Phial of Serenity
-            local phialId = 177278
-            SetMacroItem(macroIndex, GetItemInfo(phialId))
-        elseif covId == 2 then
+            SetMacroItem(GEN_MACRO_INDEX, GetItemInfo(PHIAL_ITEM_ID))
+        elseif PLAYER_COVENANT_ID == 2 then
             -- Venthyr
-            SetMacroSpell(macroIndex, GetSpellInfo("Door of Shadows"))
-        elseif covId == 3 then
+            SetMacroSpell(GEN_MACRO_INDEX, GetSpellInfo("Door of Shadows"))
+        elseif PLAYER_COVENANT_ID == 3 then
             -- Night Fae
-            SetMacroSpell(macroIndex, GetSpellInfo("Soulshape"))
-        elseif covId == 4 then
+            SetMacroSpell(GEN_MACRO_INDEX, GetSpellInfo("Soulshape"))
+        elseif PLAYER_COVENANT_ID == 4 then
             -- Necrolord
-            SetMacroSpell(macroIndex, GetSpellInfo("Fleshcraft"))
+            SetMacroSpell(GEN_MACRO_INDEX, GetSpellInfo("Fleshcraft"))
+        else
+            print("Unhandled Covenant ID:", PLAYER_COVENANT_ID)
         end
     end
 end
-
--- Macro event listeners
-local macroFrame = CreateFrame("Frame")
-macroFrame:RegisterEvent("ADDON_LOADED")
-macroFrame:RegisterEvent("COVENANT_CHOSEN")
-macroFrame:RegisterEvent("SPELLS_CHANGED")
-macroFrame:RegisterEvent("BAG_UPDATE_DELAYED")
-macroFrame:RegisterEvent("UPDATE_MACROS")
-macroFrame:SetScript("OnEvent", Mod.UpdateGeneralCovenantMacroIcon)
